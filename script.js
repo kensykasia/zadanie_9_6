0d79eaddd3cc9893074edb22ea26dc4e63963d15
Katarzyna Kensy <zbierek@gmail.com>
2018-04-19 23:26:28 +0200

    Change script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 
-add.addEventlistener('click', function() { var element = document.createElement('li');
-element.innerHTML = 'item';
-element.getElementsByTagName('li').length;
-list.appendChild(element;)
+add.addEventListener('click', function() { var element = document.createElement('li');
+element.innerHTML = 'item ' + document.getElementsByTagName('li').length;
+list.appendChild(element);
 });
